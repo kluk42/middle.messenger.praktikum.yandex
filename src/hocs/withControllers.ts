6a6d6de937267cace8e5299e10b2c879a3592917
Nikita Nemykin <nikita.nemykin@mcc-tomsk.de>
2023-05-03 16:09:28 +0700

    Rename OwnProps to props

--- src/hocs/withControllers.ts
+++ src/hocs/withControllers.ts
@@ -1,15 +1,15 @@
 import { Block, IBlock } from '../utils/Block';
 
 export function withControllers<
-  OwnProps extends Record<string, unknown>,
+  Props extends Record<string, unknown>,
   C extends Record<string, any>,
   Children extends Record<
     string,
     IBlock<Record<string, unknown>> | IBlock<Record<string, unknown>>[]
   >
->(Component: typeof Block<OwnProps, Children>, controllers: C) {
+>(Component: typeof Block<Props, Children>, controllers: C) {
   return class WithController extends Component {
-    constructor(props: Omit<OwnProps, keyof C>) {
+    constructor(props: Omit<Props, keyof C>) {
       const additionalProps = Object.entries(controllers).reduce(
         (result, [name, controller]: [keyof C, C[keyof C]]) => {
           result[name] = controller;
@@ -19,7 +19,7 @@ export function withControllers<
         {} as C
       );
 
-      super({ ...props, ...additionalProps } as OwnProps);
+      super({ ...props, ...additionalProps } as Props);
     }
   };
 }
